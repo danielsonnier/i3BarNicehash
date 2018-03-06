934d7b40b61654c0741219c226932b7f386a6b5b
Daniel Sonnier <sonnier.daniel@gmail.com>
2018-03-05 20:49:28 -0600

    fetches total hashrate

--- i3BarNicehash.py
+++ i3BarNicehash.py
@@ -2,5 +2,11 @@ import urllib, json
 url = "https://api.nicehash.com/api?method=stats.provider&addr=14SfhdUgw1gAfimCyATWcU5BxqxoqH9mWP&algo=3"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
-print data
 
+
+sum = 0
+
+for x in data['result']['stats']:
+    sum += float(x['accepted_speed'])
+
+print sum * 1000
